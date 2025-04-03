7836cf04bd90c215794df2c0f3216e9ecc5115be
simonmwaura <simonmwaura2090@gmail.com>
2025-04-03 08:07:57 +0300

    edited some filesd and did readme

--- CLI/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-   if choice=="1":
-            Client_name = input("Enter your name: ")
-            Client_email = input("Enter your email: ")
-            Client_phone_number = input("Enter your phone number: ")
-            Client_identity_number = input("Enter your identity number: ")
-            Client_id = client.create_client(Client_name, Client_email, Client_phone_number, Client_identity_number)
-            print(f"\nClient with id {Client_id} was added successfully")
